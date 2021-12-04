be26bd9effbf5abb7a321973d0493fc5d7b4a3d0
Nicholas Shephard <shephardn@gmail.com>
2021-12-04 10:15:05 -0600

    commit tree 996ab45f8e2e99d2c2939d5a6ccb10d203c89225

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,18 +4,63 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+  // consolidate forms to one State
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+    faveFood: "Pizza",
+    coolGuy: false
+  })
+
+  function handleFormChange(event) {
+    console.log(event.target.name, event.target.value)
+
+    const name = event.target.name;
+    let value = event.target.value;
+
+    if (event.target.type === "checkbox") value = event.target.checked
+    // event.target.type === "checkbox" ? value = event.target.checked : console.log("not checkbox")
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    console.log(formData)
   }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+  // function handleLastNameChange(event) {
+  //   setFormData({
+  //     ...formData,
+  //     lastName: event.target.value
+  //   });
+  // }
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <input 
+      type="text" 
+      name="firstName"
+      onChange={handleFormChange} 
+      value={formData.firstName} 
+      />
+      <input 
+      type="text" 
+      name="lastName"
+      onChange={handleFormChange} 
+      value={formData.lastName} 
+      />
+            <input 
+      type="text" 
+      name="faveFood"
+      onChange={handleFormChange} 
+      value={formData.faveFood} 
+      />
+      <input 
+      type="checkbox" 
+      name="coolGuy"
+      onChange={handleFormChange} 
+      value={formData.coolGuy} 
+      />       
       <button type="submit">Submit</button>
     </form>
   );
